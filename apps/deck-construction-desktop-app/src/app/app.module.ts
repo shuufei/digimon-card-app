01b0c17164356a67aae1bf4fa13d61435de3742a
shuufei <digital.shuufei@gmail.com>
2021-05-29 10:41:28 +0900

    setup taigaa-ui

--- apps/deck-construction-desktop-app/src/app/app.module.ts
+++ apps/deck-construction-desktop-app/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TuiButtonModule, TuiRootModule } from '@taiga-ui/core';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    TuiRootModule,
+    TuiButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
