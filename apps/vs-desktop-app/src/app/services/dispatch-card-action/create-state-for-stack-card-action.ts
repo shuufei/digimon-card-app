9894374d8ea7300bed8681ed6303334186f5b6af
shuufei <digital.shuufei@gmail.com>
2021-06-27 17:09:19 +0900

    shuffleロジック改良

--- apps/vs-desktop-app/src/app/services/dispatch-card-action/create-state-for-stack-card-action.ts
+++ apps/vs-desktop-app/src/app/services/dispatch-card-action/create-state-for-stack-card-action.ts
@@ -1,6 +1,7 @@
 import { produce } from 'immer';
 import * as _ from 'lodash';
 import { GlobalState } from '../../global-state';
+import { Card } from '../../types';
 import { StateAction } from './dispatch-card-action.service';
 
 export const createStateForStackCardAction = (
@@ -34,11 +35,27 @@ const onDraw = (currentState: GlobalState): GlobalState => {
   });
 };
 
+const shuffle = (cardList: Card[]) => {
+  const splitedStack = cardList.reduce((acc, curr, index) => {
+    const tmpStackIndex = (index / 10).toFixed(1).toString().slice(-1);
+    acc[tmpStackIndex] =
+      acc[tmpStackIndex] == null ? [curr] : [...acc[tmpStackIndex], curr];
+    return acc;
+  }, {} as Record<string, Card[]>);
+  const stackCardList = Object.keys(splitedStack).reduce((acc, curr) => {
+    return [...acc, ...splitedStack[curr]];
+  }, [] as Card[]);
+  return stackCardList;
+};
+
 const onShuffle = (currentState: GlobalState): GlobalState => {
   return produce(currentState, (draft) => {
-    draft.playState.stack.cardList = new Array(10)
+    draft.playState.stack.cardList = new Array(5)
       .fill(1)
-      .reduce((acc) => _.shuffle(acc), currentState.playState.stack.cardList);
+      .reduce(
+        (acc) => shuffle(acc),
+        _.shuffle(currentState.playState.stack.cardList)
+      );
   });
 };
 
