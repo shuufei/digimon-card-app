03574ab53a94c403b485d35d8977be71f4540165
shuufei <digital.shuufei@gmail.com>
2022-02-27 22:38:04 +0900

    feat: GET /credentials APIの実装を追加

---
+++ apps/api/get-credentials/src/assets/scripts/sign.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu
+
+readonly DISTRIBUTION_ENDPOINT="https://d2399fwvfjwbi3.cloudfront.net"
+# 1時間後に失効
+readonly EXPIRED=$(($(date +%s) + 60*60*24))
+readonly POLICY_STATEMENT="{\"Statement\":[{\"Resource\":\"${DISTRIBUTION_ENDPOINT}/*\",\"Condition\":{\"DateLessThan\":{\"AWS:EpochTime\":${EXPIRED}}}}]}"
+readonly PRIVATE_KEY="$(dirname $0)/key-pair/private_key.pem"
+
+echo $POLICY_STATEMENT
+
+readonly POLICY=$(echo $POLICY_STATEMENT | base64 | tr -- '+=/' '-_~')
+readonly SIGNATURE=$(echo $POLICY_STATEMENT | openssl sha1 -sign $PRIVATE_KEY| openssl base64 -A | tr -- '+=/' '-_~')
+
+readonly QUERY_STRINGS="Policy=${POLICY}&Signature=${SIGNATURE}&Key-Pair-Id=K3E7SLCXVOGN8A"
+
+echo "${QUERY_STRINGS}"
--- apps/api/get-credentials/src/main.ts
+++ apps/api/get-credentials/src/main.ts
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+import { execSync } from 'child_process';
+
+type Body = {
+  credentials: string;
+};
+
+const handler = async () => {
+  const stdout = execSync(`sh ${__dirname}/assets/scripts/sign.sh`);
+  const credentials = stdout.toString();
+  const body: Body = {
+    credentials,
+  };
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(body),
+  };
+  console.info('credentials: ', credentials);
+  console.info('response: ', response);
+  return response;
+};
+
+handler();
