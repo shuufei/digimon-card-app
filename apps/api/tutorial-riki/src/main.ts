7d0c929215a7d4a2619d276b452089a202e242ba
riki-hanashiro <flower.rh0119@gmail.com>
2022-05-05 16:04:09 +0900

    tutorial1_deck_name

--- apps/api/tutorial-riki/src/main.ts
+++ apps/api/tutorial-riki/src/main.ts
@@ -7,6 +7,38 @@ import * as express from 'express';
 
 const app = express();
 
+type Card = {
+  id: string;
+  name: string;
+};
+type Deck = {
+  id: string;
+  name: string;
+  cards: Card[];
+};
+
+let DeckData = [
+  {
+    id : '00',
+    name : 'アルファモン',
+    cards : require("/Users/hanakappa/Library/Mobile Documents/com~apple~CloudDocs/20210529_digimon-card-game/deck/deck_riki/アルファモン.json")
+  },
+  {
+    id : '01',
+    name : 'ジエスモン',
+    cards : require("/Users/hanakappa/Library/Mobile Documents/com~apple~CloudDocs/20210529_digimon-card-game/deck/deck_riki/ジエスモン .json")
+  }
+]
+
+app.get('/api', (req, res)=>{
+  let DeckNameList : string[] ;
+  DeckData.forEach((value, index, arr)=> {
+    DeckNameList[index] = value.name ;
+  });
+  res.send({message: DeckNameList});
+});
+
+
 app.get('/api', (req, res) => {
   res.status(400).send({ message: 'Welcome to api/tutorial-riki!!!' });
 });
