0278d2d09d5dc8e6b59c4b790f15104f31bdb2a1
shuufei <digital.shuufei@gmail.com>
2022-05-08 17:17:49 +0900

    refactor: include menu into vsscreencard

--- apps/mobile-app/src/app/screen/vs/components/presentation/deck-open-area-card.tsx
+++ apps/mobile-app/src/app/screen/vs/components/presentation/deck-open-area-card.tsx
@@ -1,50 +1,18 @@
-import { NavigationProp, useNavigation } from '@react-navigation/native';
-import { Menu, Pressable, View } from 'native-base';
 import { FC } from 'react';
-import { useSelector } from 'react-redux';
 import { CardInfo } from '../../../../domains/card';
-import { useCustomMenuProps } from '../../../../hooks/use-custom-menu-props';
-import { RootParamList } from '../../../../navigation/index';
-import * as authStore from '../../../../store/auth-store';
-import { getCardImageSrc } from '../../../../utils/get-card-image-src';
-import { VsScreenCard } from './vs-screen-card';
+import { MenuProps, VsScreenCard } from './vs-screen-card';
+
+const DECK_OPEN_AREA_MENU: MenuProps[] = [
+  { label: '登場' },
+  { label: '進化' },
+  { label: '進化元に追加' },
+  { label: '破棄' },
+  { label: '手札に追加' },
+  { label: '山札の下に戻す' },
+  { label: '山札の上に戻す' },
+  { label: 'セキュリティに追加' },
+];
 
 export const DeckOpenAreaCard: FC<{ card: CardInfo }> = ({ card }) => {
-  const signedQueryStrings = useSelector(
-    authStore.selectors.signedQueryStringsSelector
-  );
-  const [menuProps, triggerStyleProps] = useCustomMenuProps();
-  const { navigate } = useNavigation<NavigationProp<RootParamList>>();
-
-  return signedQueryStrings ? (
-    <Menu
-      {...menuProps}
-      trigger={(triggerProps) => {
-        return (
-          <Pressable
-            {...triggerProps}
-            onLongPress={() => {
-              navigate('CardModal', {
-                name: card.name,
-                cardImageSrc: getCardImageSrc(card, signedQueryStrings),
-              });
-            }}
-          >
-            <View {...triggerStyleProps}>
-              <VsScreenCard card={card} />
-            </View>
-          </Pressable>
-        );
-      }}
-    >
-      <Menu.Item>登場</Menu.Item>
-      <Menu.Item>進化</Menu.Item>
-      <Menu.Item>進化元に追加</Menu.Item>
-      <Menu.Item>破棄</Menu.Item>
-      <Menu.Item>手札に追加</Menu.Item>
-      <Menu.Item>山札の下に戻す</Menu.Item>
-      <Menu.Item>山札の上に戻す</Menu.Item>
-      <Menu.Item>セキュリティに追加</Menu.Item>
-    </Menu>
-  ) : null;
+  return <VsScreenCard card={card} menuList={DECK_OPEN_AREA_MENU} />;
 };
--- apps/mobile-app/src/app/screen/vs/components/presentation/vs-screen-card.tsx
+++ apps/mobile-app/src/app/screen/vs/components/presentation/vs-screen-card.tsx
@@ -1,22 +1,60 @@
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { Menu, Pressable, View } from 'native-base';
 import { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { Card } from '../../../../components/presentation/card';
 import { CardInfo } from '../../../../domains/card';
+import { useCustomMenuProps } from '../../../../hooks/use-custom-menu-props';
+import { RootParamList } from '../../../../navigation/index';
 import * as authStore from '../../../../store/auth-store';
+import { getCardImageSrc } from '../../../../utils/get-card-image-src';
 import { CARD_HEIGHT, CARD_WIDTH } from '../../configs/card-style';
 
-export const VsScreenCard: FC<{ card: CardInfo }> = ({ card }) => {
+export type MenuProps = {
+  label: string;
+  onPress?: () => void;
+};
+
+export const VsScreenCard: FC<{ card: CardInfo; menuList: MenuProps[] }> = ({
+  card,
+  menuList,
+}) => {
   const signedQueryStrings = useSelector(
     authStore.selectors.signedQueryStringsSelector
   );
+  const [menuProps, triggerStyleProps] = useCustomMenuProps();
+  const { navigate } = useNavigation<NavigationProp<RootParamList>>();
 
-  return (
-    <Card
-      card={card}
-      height={CARD_HEIGHT}
-      width={CARD_WIDTH}
-      isPressable={false}
-      signedQueryStrings={signedQueryStrings}
-    />
-  );
+  return signedQueryStrings ? (
+    <Menu
+      {...menuProps}
+      trigger={(triggerProps) => {
+        return (
+          <Pressable
+            {...triggerProps}
+            onLongPress={() => {
+              navigate('CardModal', {
+                name: card.name,
+                cardImageSrc: getCardImageSrc(card, signedQueryStrings),
+              });
+            }}
+          >
+            <View {...triggerStyleProps}>
+              <Card
+                card={card}
+                height={CARD_HEIGHT}
+                width={CARD_WIDTH}
+                isPressable={false}
+                signedQueryStrings={signedQueryStrings}
+              />
+            </View>
+          </Pressable>
+        );
+      }}
+    >
+      {menuList.map((menu) => {
+        return <Menu.Item onPress={menu.onPress}>{menu.label}</Menu.Item>;
+      })}
+    </Menu>
+  ) : null;
 };
