84dc8868a422f3aaff0f1bb63f1fea3f29d6ce0c
shuufei <digital.shuufei@gmail.com>
2022-05-19 22:40:43 +0900

    feat: add memory

--- apps/mobile-app/src/app/screen/vs/vs-screen.tsx
+++ apps/mobile-app/src/app/screen/vs/vs-screen.tsx
@@ -5,6 +5,7 @@ import { DeckArea } from './components/deck-area';
 import { DeckOpenArea } from './components/deck-open-area';
 import { DigitamaArea } from './components/digitama-area';
 import { HandAreaSheet } from './components/hand-area-sheet';
+import { Memory } from './components/memory';
 import { SecurityArea } from './components/security-area';
 import { SecurityCheckAreaSheet } from './components/security-check-area-sheet';
 import { SecurityOpenArea } from './components/security-open-area';
@@ -19,33 +20,40 @@ export const VSScreen = () => {
     <>
       <ScrollView>
         <VStack>
-          <HStack justifyContent={'flex-start'} p={2}>
-            <SecurityOpenArea />
-          </HStack>
-          <HStack justifyContent={'flex-end'} p={2}>
-            <DeckOpenArea />
-          </HStack>
-        </VStack>
-        <HStack justifyContent={'space-between'} mt={4} pb={'240'}>
-          <VStack>
-            <View marginLeft={'-8'}>
-              <SecurityArea />
-            </View>
-            <VStack marginLeft={'-4'} marginTop={2} space={1}>
-              <DigitamaArea />
-              <TrainingArea />
-            </VStack>
-          </VStack>
-          <View flex={1}>
-            <BattleArea />
+          <View p={2}>
+            <Memory />
           </View>
           <View>
-            <VStack marginRight={'-4'} space={2}>
-              <DeckArea />
-              <TrashArea />
+            <VStack>
+              <HStack justifyContent={'flex-start'} p={2}>
+                <SecurityOpenArea />
+              </HStack>
+              <HStack justifyContent={'flex-end'} p={2}>
+                <DeckOpenArea />
+              </HStack>
             </VStack>
+            <HStack justifyContent={'space-between'} mt={4} pb={'240'}>
+              <VStack>
+                <View marginLeft={'-8'}>
+                  <SecurityArea />
+                </View>
+                <VStack marginLeft={'-4'} marginTop={2} space={1}>
+                  <DigitamaArea />
+                  <TrainingArea />
+                </VStack>
+              </VStack>
+              <View flex={1}>
+                <BattleArea />
+              </View>
+              <View>
+                <VStack marginRight={'-4'} space={2}>
+                  <DeckArea />
+                  <TrashArea />
+                </VStack>
+              </View>
+            </HStack>
           </View>
-        </HStack>
+        </VStack>
       </ScrollView>
       <HandAreaSheet />
       <SecurityCheckAreaSheet />
